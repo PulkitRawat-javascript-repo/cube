72baa794974ba545949162c22f089fd15ef42467
PulkitRawat <p_rawat@ce.iitr.ac.in>
2023-04-06 14:23:00 +0530

    shadow

--- cube.js
+++ cube.js
@@ -32,43 +32,49 @@ plane.position.y = -2.7;
 
 
 const plane1 = new THREE.Mesh(new THREE.PlaneGeometry(5,5,5),new THREE.MeshPhongMaterial({color:0x00ff00,side:THREE.DoubleSide}))
-// plane.castShadow = true;
+plane1.castShadow = true;
+// plane1.receiveShadow = true;
 plane1.rotation.x=-Math.PI/2
 plane1.position.y = -2.5; 
 planes.push(plane1)
 scene.add(plane1)
 
 const plane2 = new THREE.Mesh(new THREE.PlaneGeometry(5,5,5),new THREE.MeshPhongMaterial({color:0x00ff00,side:THREE.DoubleSide}))
-// plane.castShadow = true;
+plane2.castShadow = true;
+// plane2.receiveShadow = true;
 plane2.position.z = 2.5;
 planes.push(plane2)
 scene.add(plane2)
 
 const plane3 = new THREE.Mesh(new THREE.PlaneGeometry(5,5,5),new THREE.MeshPhongMaterial({color:0x00ff00,side:THREE.DoubleSide}))
-// plane.castShadow = true;
+plane3.castShadow = true;
+// plane3.receiveShadow = true;
 plane3.position.z = -2.5;
 planes.push(plane3)
 scene.add(plane3)
 const plane4 = new THREE.Mesh(new THREE.PlaneGeometry(5,5,5),new THREE.MeshPhongMaterial({color:0x00ff00,side:THREE.DoubleSide}))
-// plane.castShadow = true;
+plane4.castShadow = true;
+// plane4.receiveShadow = true;
 plane4.rotation.y=-Math.PI/2
 plane4.position.x = -2.5;
 planes.push(plane4)
 scene.add(plane4)
 const plane5 = new THREE.Mesh(new THREE.PlaneGeometry(5,5,5),new THREE.MeshPhongMaterial({color:0x00ff00,side:THREE.DoubleSide}))
-// plane.castShadow = true;
+plane5.castShadow = true;
+// plane5.receiveShadow = true;
 plane5.rotation.y=Math.PI/2
 plane5.position.x = 2.5;
 planes.push(plane5)
 scene.add(plane5)
 const plane6 = new THREE.Mesh(new THREE.PlaneGeometry(5,5,5),new THREE.MeshPhongMaterial({color:0x00ff00,side:THREE.DoubleSide}))
-// plane.castShadow = true;
+plane6.castShadow = true;
+// plane6.receiveShadow = true;
 plane6.rotation.x=-Math.PI/2
 plane6.position.y = 2.5;
 planes.push(plane6)
 scene.add(plane6)
 
-const directionalLight = new THREE.DirectionalLight(0x00ff00,0.9);
+const directionalLight = new THREE.DirectionalLight(0x00ff00,0.95);
 directionalLight.position.x += 20;
 directionalLight.position.y += 20;
 directionalLight.position.z += 20;
